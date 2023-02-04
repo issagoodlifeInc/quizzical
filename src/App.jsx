f3132dc8780645026333e84426f06773719d6726
Lesley <leskimuti@gmail.com>
2023-02-04 15:34:31 +0300

    Adding nanoid and geting data from opendb API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Start from "./components/Start";
 import Quiz from "./components/Quiz";
+import { nanoid } from "nanoid";
 
 function App() {
+  // State to handle the quiz
   const [startGame, setStartGame] = useState(true);
-  console.log(startGame);
+  const [questions, setQuestions] = useState([]);
+  const [playAgain, setPlayAgain] = useState(false);
+
+  React.useEffect(() => {
+    async function getData() {
+      const res = await fetch(
+        "https://opentdb.com/api.php?amount=5&type=multiple"
+      );
+      const data = await res.json();
+
+      const newData = data.results.map((quiz) => {
+        let answersArray = [];
+        answersArray.push(quiz.correct_answer);
+        answersArray.push(...quiz.incorrect_answers);
+        return {
+          id: nanoid(),
+          question: quiz.question,
+          answer: quiz.correct_answer,
+          answers: answersArray.sort(() => Math.random() - 0.5),
+          isSelected: false,
+        };
+      });
+      setQuestions(newData);
+    }
+
+    getData();
+    // console.log(getData());
+  }, [playAgain]);
+
+  console.log(questions);
+
+  const quizElements = questions.map((quiz) => {
+    return (
+      <Quiz key={quiz.id} question={quiz.question} answers={quiz.answers} />
+    );
+  });
+
   return (
     <main className="App">
       <Start start={startGame} handleStart={() => setStartGame(!startGame)} />
-      <Quiz start={startGame} />
+      {!startGame && (
+        <section className="container ">
+          {quizElements};<button className="btn__check">Check Answers</button>
+        </section>
+      )}
     </main>
   );
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,50 +1,10 @@
-export default function Quiz({ start }) {
+export default function Quiz({ question, answers }) {
   return (
-    !start && (
-      <section className="container ">
-        <div className="question">
-          <h3>How would one say goodbye in Spanish?</h3>
-          <button className="btn__quiz">Adiós</button>
-          <button className="btn__quiz">Hola</button>
-          <button className="btn__quiz">Au Revoir</button>
-          <button className="btn__quiz">Salir</button>
-        </div>
-        <div className="question">
-          <h3>
-            Which best selling toy of 1983 caused hysteria, resulting in riots
-            breaking in stores?
-          </h3>
-          <button className="btn__quiz">Cabbage Patch Kids</button>
-          <button className="btn__quiz">Transformers</button>
-          <button className="btn__quiz">Au Revoir</button>
-          <button className="btn__quiz">Care Bears</button>
-        </div>
-        <div className="question">
-          <h3>How would one say goodbye in Spanish?</h3>
-          <button className="btn__quiz">Adiós</button>
-          <button className="btn__quiz">Hola</button>
-          <button className="btn__quiz">Au Revoir</button>
-          <button className="btn__quiz">Salir</button>
-        </div>
-        <div className="question">
-          <h3>How would one say goodbye in Spanish?</h3>
-          <button className="btn__quiz">Adiós</button>
-          <button className="btn__quiz">Hola</button>
-          <button className="btn__quiz">Au Revoir</button>
-          <button className="btn__quiz">Salir</button>
-        </div>
-        <div className="question">
-          <h3>How would one say goodbye in Spanish?</h3>
-          <button className="btn__quiz">Adiós</button>
-          <button className="btn__quiz">Hola</button>
-          <button className="btn__quiz">Au Revoir</button>
-          <button className="btn__quiz">Salir</button>
-        </div>
-
-        {/* Answers */}
-
-        <button className="btn__check">Check Answers</button>
-      </section>
-    )
+    <div className="question">
+      <h3>{question}</h3>
+      {answers.map((ans) => (
+        <button className="btn__quiz">&#039;{ans}</button>
+      ))}
+    </div>
   );
 }
