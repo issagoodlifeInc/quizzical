0ef273068f3dccf83daa921a78b592a7a111d79a
Lesley <leskimuti@gmail.com>
2023-02-05 10:37:24 +0300

    Added some description

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,7 +3,7 @@ export default function Start({ start, handleStart }) {
     start && (
       <div className="container start">
         <h1>Quizzical</h1>
-        <p>Some description if needed</p>
+        <p>Five random questions with multiple choices about anything </p>
         <button className="btn btn__start" onClick={handleStart}>
           Start quiz
         </button>
