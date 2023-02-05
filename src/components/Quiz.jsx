ad6498246e349a91580103d1e87bc8f067274e73
Lesley <leskimuti@gmail.com>
2023-02-05 09:57:59 +0300

    Separated the quiz to require the answer coponent

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,21 @@
-export default function Quiz({ question, answers }) {
+import Answer from "./Answer";
+
+export default function Quiz({ question, answers, selectAnswer, score }) {
+  const answerElements = answers.map((ans) => (
+    <Answer
+      key={ans.id}
+      choice={ans.choice}
+      score={score}
+      isSelected={ans.isSelected}
+      isCorrect={ans.isCorrect}
+      handleClick={selectAnswer}
+    />
+  ));
+
   return (
     <div className="question">
       <h3>{question}</h3>
-      {answers.map((ans) => (
-        <button className="btn__quiz">&#039;{ans}</button>
-      ))}
+      {answerElements}
     </div>
   );
 }
